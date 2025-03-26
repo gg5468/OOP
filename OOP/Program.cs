e8b6096a17b3131d671856cda5a92378591cc75f
gg5468 <gg5468@email.com>
2025-03-26 19:10:20 +0100

    Resolved Aggregation.

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -1,29 +1,33 @@
 ﻿using OOP.Abstraction;
+using OOP.Aggregation;
 using OOP.Composition;
 using OOP.Encapsulation;
 
 
 // Encapsulation
-//BadBankAccountServiceExample.Main();
-//BankAccountServiceExample.Main();
+BadBankAccountServiceExample.Main();
+BankAccountServiceExample.Main();
 
 // Abstraction
-//BadEmailServiceExample.Main();
-//EmailServiceExample.Main();
+BadEmailServiceExample.Main();
+EmailServiceExample.Main();
 
 // Inheritance
-//OOP.Inheritance.BadExample.BadProgram.Main();
-//OOP.Inheritance.GoodExample.GoodProgram.Main();
+OOP.Inheritance.BadExample.BadProgram.Main();
+OOP.Inheritance.GoodExample.GoodProgram.Main();
 
 // Polymorphism
-//OOP.Polymorphism.BadExample.BadProgram.Main();
-//OOP.Polymorphism.GoodExample.GoodProgram.Main();
+OOP.Polymorphism.BadExample.BadProgram.Main();
+OOP.Polymorphism.GoodExample.GoodProgram.Main();
 OOP.Polymorphism.MethodOverloading.Main();
 OOP.Polymorphism.OperatorOverloading.Main();
 
 // Coupling
-//OOP.Coupling.BadExample.BadProgram.Main();
-//OOP.Coupling.GoodExample.GoodProgram.Main();
+OOP.Coupling.BadExample.BadProgram.Main();
+OOP.Coupling.GoodExample.GoodProgram.Main();
 
 // Composition
-//CarExample.Main();
+CarExample.Main();
+
+// Aggregation
+Aggregation.Main();
